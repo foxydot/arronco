93e958cd3dfd6b962be8b8c031ae3f1a6045d95e
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-03-20 17:31:01 -0400

    fix testimonial metabox input

--- assets/plugins/msd-custom-cpt/lib/template/testimonial-information.php
+++ assets/plugins/msd-custom-cpt/lib/template/testimonial-information.php
@@ -1,10 +1,8 @@
 <?php global $wpalchemy_media_access; 
 ?>
 
-<div class="meta_control">
-    <div class="table">
-    <div class="row">
-        <div class="cell">
+<ul class="meta_control">
+    <li>
             <label>Quote</label>
             <div class="input_container">
                 <?php 
@@ -15,40 +13,32 @@
                 wp_editor( $mb_content, $mb_editor_id, $mb_settings );
                 ?>
            </div>
-        </div>
-    </div>
+        </li>
 
-    <div class="row">
-        <div class="cell">
+    <li>
         <?php $metabox->the_field('attribution'); ?>
             <label>Attribution Name</label>
             <div class="input_container">
                 <input type="text" value="<?php $metabox->the_value(); ?>" id="<?php $metabox->the_name(); ?>" name="<?php $metabox->the_name(); ?>">
            </div>
-        </div>
-    </div>
+        </li>
     
-    <div class="row">
-        <div class="cell">
+    <li>
         <?php $metabox->the_field('position'); ?>
             <label>Attribution Position</label>
             <div class="input_container">
                 <input type="text" value="<?php $metabox->the_value(); ?>" id="<?php $metabox->the_name(); ?>" name="<?php $metabox->the_name(); ?>">
            </div>
-        </div>
-    </div>
+        </li>
     
-    <div class="row">
-        <div class="cell">
+    <li>
         <?php $metabox->the_field('company'); ?>
             <label>Attribution Company</label>
             <div class="input_container">
                 <input type="text" value="<?php $metabox->the_value(); ?>" id="<?php $metabox->the_name(); ?>" name="<?php $metabox->the_name(); ?>">
            </div>
-        </div>
-    </div>
-    </div>
-</div>
+        </li>
+</ul>
 <script>
 jQuery(function($){
     $("#postdivrich").after($("#_testimonial_info_metabox"));

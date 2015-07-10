e1428f38f3854ebc27b018a2caa80b94633c5417
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-07-10 15:38:59 -0400

    plugin updates

--- assets/plugins/msd_site_settings/lib/inc/widgets.php
+++ assets/plugins/msd_site_settings/lib/inc/widgets.php
@@ -7,7 +7,7 @@ class MSDConnected extends WP_Widget {
     function MSDConnected() {
 		$widget_ops = array('classname' => 'msd-connected', 'description' => __('Show social icons'));
 		$control_ops = array('width' => 400, 'height' => 350);
-		$this->WP_Widget('connected', __('Connected'), $widget_ops, $control_ops);
+		$this->WP_Widget('connected', __('MSD Connected'), $widget_ops, $control_ops);
 	}
 
 	function widget( $args, $instance ) {
@@ -154,7 +154,7 @@ class MSDAddress extends WP_Widget {
     function MSDAddress() {
 		$widget_ops = array('classname' => 'msd-address', 'description' => __('Display addresses'));
 		$control_ops = array('width' => 400, 'height' => 350);
-		$this->WP_Widget('address', __('Address'), $widget_ops, $control_ops);
+		$this->WP_Widget('address', __('MSD Address'), $widget_ops, $control_ops);
 	}
 
 	function widget( $args, $instance ) {
@@ -195,7 +195,7 @@ class MSDCopyright extends WP_Widget {
     function MSDCopyright() {
 		$widget_ops = array('classname' => 'msd-copyright', 'description' => __('Display copyright notice'));
 		$control_ops = array('width' => 400, 'height' => 350);
-		$this->WP_Widget('copyright', __('Copyright'), $widget_ops, $control_ops);
+		$this->WP_Widget('copyright', __('MSD Copyright'), $widget_ops, $control_ops);
 	}
 
 	function widget( $args, $instance ) {
@@ -228,3 +228,51 @@ class MSDCopyright extends WP_Widget {
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("MSDCopyright");'));
+
+
+class MSDVisit extends WP_Widget {
+    /** constructor */
+    function MSDVisit() {
+        $widget_ops = array('classname' => 'msd-visit', 'description' => __('Display shop hours'));
+        $control_ops = array('width' => 400, 'height' => 350);
+        $this->WP_Widget('visit', __('MSD Visit Us'), $widget_ops, $control_ops);
+    }
+
+    function widget( $args, $instance ) {
+        extract($args);
+        global $msd_social;
+        $title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance );
+        $text = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance );
+        echo $before_widget;
+        if ( !empty( $title ) ) { print $before_title.$title.$after_title; } 
+        echo '<div class="business-hours">'.$msd_social->get_hours_deux().'</div>';
+        if ( !empty( $text )){ print '<div class="business-hours-text">'.$text.'</div>'; }
+        echo $after_widget;
+    }
+
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        if ( current_user_can('unfiltered_html') )
+            $instance['text'] =  $new_instance['text'];
+        else
+            $instance['text'] = stripslashes( wp_filter_post_kses( addslashes($new_instance['text']) ) ); // wp_filter_post_kses() expects slashed
+        
+        return $instance;
+    }
+
+    function form( $instance ) {
+        $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
+        $title = strip_tags($instance['title']);
+        $text = esc_textarea($instance['text']);      
+?>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p> 
+        <p><label for="<?php echo $this->get_field_id('text'); ?>"><?php _e('Text:'); ?></label>
+        <textarea class="widefat" rows="5" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea></p>
+
+<?php
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("MSDVisit");'));
